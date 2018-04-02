c8efa4418c3e637ce87f2673078bcc16f4cb28b4
rosivanov <ros.g6w@gmail.com>
2018-04-02 14:42:24 +0300

    add package page styles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,10 @@
 # Changelog
 ---
 
-## 1.1
+## 2.0
 ---
 + Dark theme is avaliable for the next pages: Package, Search result, Documentation. Dropped support for the main page and other inner pages.
++ npmjs.com/package regex
 
 ## 1.0
 ---
--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass');
 
 gulp.task('scss', function () {
-	gulp.src('scss/style.scss')
+	gulp.src('scss2/style.scss')
 	.pipe(sass({ outputStyle: 'compressed' })) // extended
 	.pipe(gulp.dest('css/'));
 });
 
 gulp.task('watch', function() {
-	gulp.watch('scss/*.scss', ['scss'])
+	gulp.watch('scss2/*.scss', ['scss'])
 });
 
 gulp.task('default', ['scss', 'watch']);
